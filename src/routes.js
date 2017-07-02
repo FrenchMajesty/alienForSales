15bb2dde497b5adc04563fc9a59b3d9217a419c2
FrenchMajesty <verdi.kap@gmail.com>
2017-07-02 18:17:03 -0500

    [Add] Routes

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,20 @@ import App from './components/app'
 import Home from './scenes/Home'
 import AboutMe from './scenes/AboutMe'
 import Gallery from './scenes/Gallery'
+import GallerySingleView from './scenes/GallerySingleView'
 
+import Login from './scenes/Admin/Login'
 
 export default (
-    <Route path="/" component={App}>
-        <IndexRoute component={Home} />
-        <Route path="/about" component={AboutMe} />
-        <Route path="/gallery" component={Gallery} />
-    </Route>
-);
+        <Route path="/" component={App}>
+            <IndexRoute component={Home} />
+            <Route path="/about" component={AboutMe} />
+            <Route path="/gallery">
+                <IndexRoute component={Gallery} />
+                <Route path="/gallery(/:id)" component={GallerySingleView} />
+            </Route>
+            <Route path="/admin">
+                <Route path="/admin/login" component={Login} />
+            </Route>
+        </Route>
+)
