d2a4f3ba2780264da27949a37bc1bfd086a33f3f
FrenchMajesty <verdi.kap@gmail.com>
2017-07-02 14:46:30 -0500

    [Update] Minor fixes

--- src/components/ColumnContainer/components/Pager/index.js
+++ src/components/ColumnContainer/components/Pager/index.js
@@ -5,10 +5,12 @@ import BasicButton from '~/components/Button/BasicButton'
 
 const Pager = (props) => {
     
+    const newButton = "blog-pager-newer-link"
+    const oldButton = "blog-pager-older-link"
     return (
         <div className="blog-pager" id="blog-pager">
-        <BasicButton title="Newer Posts" target="#" />
-        <BasicButton title="Older Posts" target="#" />
+        <BasicButton id={newButton} className={newButton} title="Newer Posts" target="#" />
+        <BasicButton id={oldButton} className={oldButton} title="Older Posts" target="#" />
             <Link to="/" className="home-link">Home</Link>
         </div>
     )
