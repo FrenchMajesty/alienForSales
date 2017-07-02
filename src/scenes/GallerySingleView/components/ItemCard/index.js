3d819530f8f5a97b9cb6d9cd00a48069ec3b71bb
FrenchMajesty <verdi.kap@gmail.com>
2017-07-02 14:46:22 -0500

    [Add] Single View of Gallery item

---
+++ src/scenes/GallerySingleView/components/ItemCard/index.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { Link } from 'react-router'
+import BasicButton from '~/components/Button/BasicButton'
+import { formatDate } from '~/services/Helper'
+
+const ItemCard = (props) => {
+
+    const socialButtons = () => {
+        return (
+            <div className="social-share-circles">
+            <Link to="#" title="Share on Facebook" className="sc-circle fb anim-1">   <i className="fa fa-facebook"></i> </Link>
+            <Link to="#" title="Share on Google+" className="sc-circle gp anim-2">   <i className="fa fa-google-plus"></i> </Link>
+            <Link to="#" className="sc-circle tw anim-3">   <i className="fa fa-twitter"></i> </Link>
+            </div>
+        )
+    }
+
+    const renderHead = () => {
+        const {data} = props
+        return (
+            <div className="date-header single-view-date">
+                <span className="out">
+                    <i className="fa fa-fw fa-calendar"></i>
+                    <span className="dte">{formatDate(data.date)}</span>
+                </span>
+            </div>
+        )
+    }
+
+    const renderFooter = () => {
+        const {data, author} = props
+
+        return(
+            <div className="post-footer" style={{textAlign: "initial"}}>
+                <div className="post-footer-line post-footer-line-1">
+                    <span className="post-author vcard">By {author}</span>
+                    <span className="post-comment-link">
+                            {data.stock > 0 &&
+                                <Link to="#buy" className="comment-link"> {data.stock} left in stock! </Link>}
+                    </span>
+                    <div className="post-share-buttons goog-inline-block"></div>
+                </div>
+
+                <div className="post-footer-line post-footer-line-2">
+                    <span className="post-labels">Labels:
+                        {data.tags.map((tag, i) => {
+                            const comma = (i != 0) ? ', ' : ''
+                            return (<sect key={i}><i>{comma}</i><Link to="#" rel="tag">{tag}</Link></sect>)
+                        })}
+                    </span>
+                </div>
+            </div>
+        )
+    }
+
+        const {data} = props
+        return (
+            <div className="date-outer">
+
+                {renderHead()}
+
+                <div className="date-posts">
+                <div className="post-outer summarized wow fadeInLeft">
+                <div className="post hentry" itemProp="blogPost" itemScope="itemscope" itemType="http://schema.org/BlogPosting">
+                <div className="post-body entry-content" id="post-body-5393435426553883135" itemProp="description articleBody">
+
+                     <div className="media-box image">
+                        <div className="overlay-img"></div>
+                        <Link to="#" className="post-url"></Link>
+                        <img className="image" src="http://4.bp.blogspot.com/-TPrTQfHEaNw/VW_UfIuOC6I/AAAAAAAAFko/d-Iut3BR_HE/s800/dawki-35346456.jpg" />
+                        {socialButtons()}
+                    </div>
+                    <div className="content" style={{textAlign: "initial"}}>
+                        <div className="post-title-box">
+                            <h2>
+                                <Link to="#">{data.title}</Link>
+                            </h2>
+                        </div>
+                        <div className="post-meta"></div>
+                        <div className="post-summary">
+                            {data.summary}
+                        </div><br/>
+                        <center><BasicButton target="#" title="Buy now" color="#f15a24" disabled={!data.stock ? true: false} /></center>
+                       
+                    </div>
+
+                        <div className="material-post-footer">
+                            <div className="clear"></div>
+                            <div className="clear"></div>
+                        </div>
+
+                    </div>
+
+                    {renderFooter()}
+                    
+                </div>
+                </div>
+                </div>
+
+            </div>
+        )
+}
+
+export default ItemCard
---
+++ src/scenes/GallerySingleView/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router'
+import ItemCard from './components/ItemCard'
+import PageWrapper from '~/components/PageWrapper'
+import ColumnContainer from '~/components/ColumnContainer'
+
+const GallerySingleViw = (props) => {
+    
+     const data = {
+            title: props.params.id,
+            summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non ex venenatis, egestas est vitae, lacinia libero. Sed pretium aliquet...",
+            tags: ["story", "label"],
+            date: "05/30/2015",
+            stock: 10
+        }
+
+        const author = "Barrack Obama"
+    
+    return (
+        <PageWrapper>
+            <div className="main-outer">
+            <div className="fauxborder-left main-fauxborder-left">
+            <div className="region-inner main-inner">
+            <div className="columns fauxcolumns" style={{padding: "0 5em"}}>
+            <div className="columns-inner">
+
+            <ColumnContainer type="center">
+                <div className="blog-posts hfeed">
+                    <ItemCard data={data} author={author} />
+                </div>
+                </ColumnContainer>
+
+            </div>
+            <div style={{clear: "both"}} />
+            </div>
+            </div>
+            </div>
+            </div>
+        </PageWrapper>
+    )
+}
+
+export default GallerySingleViw
