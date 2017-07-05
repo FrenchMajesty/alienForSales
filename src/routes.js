99c91d27c85db798f8ea3339d84d21417d43874c
FrenchMajesty <verdi.kap@gmail.com>
2017-07-05 08:01:30 -0500

    [Add] Admin panel routes

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,13 @@ import AboutMe from './scenes/AboutMe'
 import Gallery from './scenes/Gallery'
 import GallerySingleView from './scenes/GallerySingleView'
 
+import Admin from './scenes/Admin'
 import Login from './scenes/Admin/Login'
+import Dashboard from './scenes/Admin/Dashboard'
+import EditGallery from './scenes/Admin/EditGallery'
+import AddGallery from './scenes/Admin/AddGallery'
+import AddBlog from './scenes/Admin/AddBlog'
+
 
 export default (
         <Route path="/" component={App}>
@@ -17,8 +23,12 @@ export default (
                 <IndexRoute component={Gallery} />
                 <Route path="/gallery(/:id)" component={GallerySingleView} />
             </Route>
-            <Route path="/admin">
+            <Route path="/admin" component={Admin}>
+                <Route path="/admin/index" component={Dashboard} />
                 <Route path="/admin/login" component={Login} />
+                <Route path="/admin/gallery" component={EditGallery} />
+                <Route path="/admin/gallery/add" component={AddGallery} />
+                <Route path="/admin/blogs/add" component={AddBlog} />
             </Route>
         </Route>
 )
