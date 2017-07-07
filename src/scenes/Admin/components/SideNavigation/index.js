83b0eba369e2c164ecddc7c3baf56fb6064149e7
FrenchMajesty <verdi.kap@gmail.com>
2017-07-07 07:21:37 -0500

    [Update] Minor fix

--- src/scenes/Admin/components/SideNavigation/index.js
+++ src/scenes/Admin/components/SideNavigation/index.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router'
 const SideNavigation = (props) => {
     
     const {user, active} = props
-    console.log(active.search(/^\/blogs/))
     return (
     
         <aside id="leftsidebar" className="sidebar">
