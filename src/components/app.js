da1927ac2934bde745f253a38623e34eb8f3f929
FrenchMajesty <verdi.kap@gmail.com>
2017-07-12 14:14:30 -0500

    [Add] Support for material UI

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 export default class App extends Component {
 
-  render() {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    )
-  }
+    
+    componentWillMount() {
+        injectTapEventPlugin()   
+    }
+    
+    render() {
+        return (
+            <MuiThemeProvider>
+                {this.props.children}
+            </MuiThemeProvider>
+        )
+    }
 }
