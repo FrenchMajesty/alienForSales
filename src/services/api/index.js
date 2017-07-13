a7e09662f4da6cb2e7d22a68da82fcc1e36f51e4
FrenchMajesty <verdi.kap@gmail.com>
2017-07-12 20:03:24 -0500

    [Update] Replace axios with superagent
    Axios returns a 400 bad request. After research it seems to be an
    on-going bug

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,30 +1,20 @@
-import axios from 'axios'
+import request from 'superagent'
 
 const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/verdi-co/upload'
 const CLOUDINARY_KEY = 'v75nh578'
 
 export function uploadImage(file, callback){
-    console.log(file)
-    axios.post(CLOUDINARY_URL, {
-        upload_preset: CLOUDINARY_KEY,
-        file: file
-    })
-    .then(response => callback(null, response))
-    .catch(error => callback(error))
     
-       /* let upload = request.post(URL)
-                            .field('upload_preset', KEY)
-                            .field('file', image)
+    let upload = request.post(CLOUDINARY_URL)
+                        .field('upload_preset', CLOUDINARY_KEY)
+                        .field('file', file)
         
-        upload.end((err, response) => {
-            if(err) {
-                this.setState({error: [err]})
-                return
-            }
-
-            if(response.body.secure_url == '')
-                this.setState({error: ['Your image could not be uploaded, please try again.']})
+    upload.end((err, response) => {
+        if(err) 
+            callback(err)
+        else if(response.body.secure_url == '')
+            callback('Your image could not be uploaded, please try again.')
             else
-                this.setState({uploadedFileUrl: response.body.secure_url})
-        })*/
+                callback(null, response)
+    })
 }
