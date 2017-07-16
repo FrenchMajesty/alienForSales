8b9928f12b086cd34974a89c1158033514d8213b
FrenchMajesty <verdi.kap@gmail.com>
2017-07-16 00:35:16 -0500

    Add event handlers and rending for page buttons

--- src/components/Container/ColumnContainer/Pager/index.js
+++ src/components/Container/ColumnContainer/Pager/index.js
@@ -3,14 +3,16 @@ import {Link} from 'react-router'
 import BasicButton from '~/components/Button/BasicButton'
 
 
-const Pager = (props) => {
+const Pager = ({ moveBackward, moveForward, current, max }) => {
     
     const newButton = "blog-pager-newer-link"
     const oldButton = "blog-pager-older-link"
     return (
         <div className="blog-pager" id="blog-pager">
-        <BasicButton id={newButton} title="Newer Posts" target="#" />
-        <BasicButton id={oldButton} title="Older Posts" target="#" />
+            {current[0] > 0 &&
+                <BasicButton id={newButton} title="Newer Posts" target="#" onClick={moveForward} />}
+            {current[1] < max &&
+                <BasicButton id={oldButton} title="Older Posts" onClick={moveBackward} />}
             <Link to="/" className="home-link">Home</Link>
         </div>
     )
