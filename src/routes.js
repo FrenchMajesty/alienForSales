f86a752c6d2917f89700a2b392b358e83592a790
FrenchMajesty <verdi.kap@gmail.com>
2017-07-22 11:04:01 -0500

    Remove change log scene

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,6 @@ import EditGallery from './scenes/Admin/EditGallery'
 import AddGallery from './scenes/Admin/AddGallery'
 import AddBlog from './scenes/Admin/AddBlog'
 import BlogList from './scenes/Admin/BlogList'
-import Changelog from './scenes/Admin/Changelog'
 import Settings from './scenes/Admin/Settings'
 
 const author = "Barrack Obama"
@@ -32,7 +31,6 @@ export default (
                 <Route path="/admin/gallery/add" component={AddGallery} />
                 <Route path="/admin/blogs" component={BlogList} />
                 <Route path="/admin/blogs/add" component={AddBlog} />
-                <Route path="/admin/log" component={Changelog} />
                 <Route path="/admin/settings" component={Settings} />
             </Route>
             <Route path="/admin/login" component={Login} />
