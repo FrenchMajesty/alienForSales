9c02366cf7e43a7130c21a688a218e588b14ab74
FrenchMajesty <verdi.kap@gmail.com>
2017-07-28 18:30:19 -0500

    Update to use react-facebook component

--- src/components/Container/FooterSocials/index.js
+++ src/components/Container/FooterSocials/index.js
@@ -3,14 +3,27 @@ import FlatButton from 'material-ui/FlatButton'
 import IconButton from 'material-ui/IconButton'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import {indigo600, lightBlue300} from 'material-ui/styles/colors'
+import FacebookProvider, {Share} from 'react-facebook'
 
-const FooterSocials = (props) => {
- 
+const FooterSocials = ({url}) => {
+    
+    const shareTwitter = () => {
+        // use twitter web intent   
+    }
+    
     return (
         <div className="post-share-buttons goog-inline-block" style={{marginLeft: '4em'}}>
             Share on: 
-            <IconButton iconClassName="fa fa-facebook fa-lg" tooltip="Facebook" iconStyle={{color: indigo600}} />
-            <IconButton iconClassName="fa fa-twitter fa-lg" tooltip="Twitter" iconStyle={{color: lightBlue300}} />
+            <FacebookProvider appId="1427529060645955">
+                <Share href={url}>
+                    <IconButton iconClassName="fa fa-facebook fa-lg" tooltip="Facebook" 
+                                iconStyle={{color: indigo600}}
+                    />
+                </Share>
+            </FacebookProvider>
+            <IconButton iconClassName="fa fa-twitter fa-lg" tooltip="Twitter" iconStyle={{color: lightBlue300}}
+                onTouchTap={shareTwitter}
+            />
     </div>
     )
 }
